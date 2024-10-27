745ac1b9c624c46d1d6eb09aad14229f3931c033
aleksander <147505187+baskaaleksander@users.noreply.github.com>
2024-10-27 17:51:18 +0100

    test

--- _3_dziedziczenie/obj/Debug/net8.0/_3_dziedziczenie.AssemblyInfo.cs
+++ _3_dziedziczenie/obj/Debug/net8.0/_3_dziedziczenie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_3_dziedziczenie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3d1460f1409fce04e48ea2b51111c17efd9957")]
 [assembly: System.Reflection.AssemblyProductAttribute("_3_dziedziczenie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_3_dziedziczenie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
